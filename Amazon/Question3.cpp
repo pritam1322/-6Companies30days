890adf39411c5ea29fcb2488bb76c138e0f08f5d
PRITAM PRAKASH JATHAR <pritamjat98@gmail.com>
2022-01-07 13:00:15 +0530

    Create Question3.cpp

---
+++ Amazon/Question3.cpp
@@ -0,0 +1,62 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution {
+  public:
+    vector<int> max_of_subarrays(vector<int> arr, int n, int k) {
+        
+        deque<int>q;
+        vector<int> res;
+        
+        int i = 0 ; int j = 0;
+        
+        
+        while(i<n)
+        {
+            while(!q.empty() && q.back() < arr[j])
+            {
+                q.pop_back();
+            }
+            q.push_back(arr[j]);
+            
+            if(j-i+1 == k)
+            {
+                res.push_back(q[0]);
+                
+                if(q[0] == arr[i])
+                {
+                    q.pop_front();
+                }
+            }
+        
+        }
+        return res;
+        // your code here
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+
+        int n, k;
+        cin >> n >> k;
+
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++) cin >> arr[i];
+        Solution ob;
+        vector<int> res = ob.max_of_subarrays(arr, n, k);
+        for (int i = 0; i < res.size(); i++) cout << res[i] << " ";
+        cout << endl;
+    }
+
+    return 0;
+}  // } Driver Code Ends
