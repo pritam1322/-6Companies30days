6cb91102495addfb7c0c60866abf02fc613f4c98
PRITAM PRAKASH JATHAR <pritamjat98@gmail.com>
2022-01-09 21:55:09 +0530

    Create Question6.py

---
+++ Amazon/Question6.py
@@ -0,0 +1,77 @@
+#User function Template for python3
+
+class Solution:
+    def max_of_subarrays(self,arr,n,k):
+        '''
+        you can use collections module here.
+        :param a: given array
+        :param n: size of array
+        :param k: value of k
+        :return: A list of required values 
+        
+        if cur ele is greater than elements in stack just pop() them and add cur ele to stack.
+        if the sliding widow hits size == k then push top ele of stack in res.
+        
+        '''
+        #code here
+        
+        i = 0 
+        j = 0
+        m = 0
+        res = []
+        stack = [0]
+        
+        while j < n:
+            
+            while len(stack)>0 and stack[-1] < arr[j]:
+                stack.pop()
+                
+            stack.append(arr[j])
+            
+            
+            if j-i+1 == k:
+                res.append(stack[0])
+               
+                if stack[0] == arr[i]:
+                    stack.pop(0)
+                i += 1
+               
+                   
+               
+            j += 1
+               
+               
+        return res
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+import atexit
+import io
+import sys
+from collections import deque
+
+#Contributed by : Nagendra Jha
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases) :
+        n,k = map(int,input().strip().split())
+        arr = list(map(int,input().strip().split()))
+        ob=Solution()
+        res = ob.max_of_subarrays(arr,n,k)
+        for i in range (len (res)):
+            print (res[i], end = " ")
+        print()
+# } Driver Code Ends
