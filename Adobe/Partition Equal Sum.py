1af8f0eaaa8195cbe87839ff59ccf96d6130a9c1
PRITAM PRAKASH JATHAR <pritamjat98@gmail.com>
2022-01-17 09:55:00 +0530

    Create Partition Equal Sum.py

---
+++ Adobe/Partition Equal Sum.py
@@ -0,0 +1,51 @@
+#User function Template for python3
+
+#Function to find a continuous sub-array which adds up to a given number.
+class Solution:
+    def subArraySum(self,arr, n, s): 
+       #Write your code here
+        i = 0
+        j = 0
+        total = 0
+       
+        while j < n:
+            total += arr[j]
+           
+            while total > s:
+                total -= arr[i]
+                i += 1
+            
+            if total == s:
+                return [i+1,j+1]
+            j += 1
+            
+        return [-1]
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+def main():
+        T=int(input())
+        while(T>0):
+            
+            NS=input().strip().split()
+            N=int(NS[0])
+            S=int(NS[1])
+            
+            A=list(map(int,input().split()))
+            ob=Solution()
+            ans=ob.subArraySum(A, N, S)
+            
+            for i in ans:
+                print(i, end=" ")
+                
+            print()
+            
+            T-=1
+
+
+if __name__ == "__main__":
+    main()
+# } Driver Code Ends
