7acbd8841ac4ce4732520b90e483491aa06b5f6d
PRITAM PRAKASH JATHAR <pritamjat98@gmail.com>
2022-01-24 20:20:47 +0530

    Update Course Schedule II.py

--- Intuit/Course Schedule II.py
+++ Intuit/Course Schedule II.py
@@ -1,5 +1,43 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+       """
+       Kahn's Algo
+        graph = [[] for _ in range(numCourses)]
+        indegree = [0] * numCourses
+        
+        ans = []
+        
+        if len(prerequisites) == 0:
+            return [i for i in range(numCourses)]
+        
+        
+        for u,v in prerequisites:
+            graph[v].append(u)
+            indegree[u] += 1
+            
+        q = []
+        
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                q.append(i)
+        
+        
+        while q:
+            node = q.pop(0)
+            
+            ans.append(node)
+            
+            for nei in graph[node]:
+                indegree[nei] -= 1
+                
+                if indegree[nei] == 0 :
+                    q.append(nei)
+                    
+        return ans if len(ans) == numCourses else []
+    """
+        
+        
+        
         graph = [[] for _ in range(numCourses)]
         
         for u,v in prerequisites:
